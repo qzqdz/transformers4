56f32bf41e83d634e97bf85c39c9aa25c303bb8c
qzqdz <100898934+qzqdz@users.noreply.github.com>
2022-10-08 23:19:31 +0800

    some train order changed in c2

--- train_code.py
+++ train_code.py
@@ -281,9 +281,13 @@ python ./examples/pytorch/text-classification/run_glue_no_trainer.py     --model
 bertcb_lr20_bs8_256_lv1
 python ./examples/pytorch/text-classification/run_glue_no_trainer.py     --model_name_or_path D:/model/white_model/bert_base_uncased     --train_file D:/data/nlpcct5/nlpcct5.py  --max_length 256   --do_train True  --report_to wandb    --with_tracking --per_device_eval_batch_size 8  --per_device_train_batch_size 8  --learning_rate 2e-5     --num_train_epochs 3   --output_dir D:/model/transformers4/bertcb_lr20_bs8_256_lv1/ --checkpointing_steps 1000
 
+bertntrfl_lr20_bs8_256_lv1
+python ./examples/pytorch/text-classification/run_glue_no_trainer.py     --model_name_or_path D:/model/white_model/bert_base_uncased     --train_file D:/data/nlpcct5/nlpcct5.py  --max_length 256   --do_train True  --report_to wandb    --with_tracking --per_device_eval_batch_size 8  --per_device_train_batch_size 8  --learning_rate 2e-5     --num_train_epochs 3   --output_dir D:/model/transformers4/bertntrfl_lr20_bs8_256_lv1/ --checkpointing_steps 1000
 
-D:/model/white_model/bert_base_uncased
-D:/model/transformers4/bertrdrop50_lr20_bs8_256_lv1
+
+bertcbloss_lr20_bs8_256_lv1
+python ./examples/pytorch/text-classification/run_glue_no_trainer.py     --model_name_or_path D:/model/white_model/bert_base_uncased     --train_file D:/data/nlpcct5/nlpcct5.py  --max_length 256   --do_train True  --report_to wandb    --with_tracking --per_device_eval_batch_size 8  --per_device_train_batch_size 8  --learning_rate 2e-5     --num_train_epochs 3   --output_dir D:/model/transformers4/bertcbloss_lr20_bs8_256_lv1/ --checkpointing_steps 1000
+
 
 
 '''
